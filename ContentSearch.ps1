fe40ee73dd82f117163a7973379d823d225446a9
Kaneabell <liang1988@live.cn>
2014-03-13 14:53:29 +0800

    Add some powershell scripts for learning git"

---
+++ ContentSearch.ps1
@@ -0,0 +1,25 @@
+param(
+	[parameter(mandatory = $true)]
+	[string]$regexpattern, 
+	[string]$searchpath = $pwd,
+	[string]$filetype = "*.*"
+	)
+
+function contentsearch ($pattern, $path, $type)
+{
+	$dir = $pwd
+	cd $path
+	$files = get-childitem -recurse | where { $_.name -like $type }
+	foreach ($file in $files)
+	{
+		$content = [system.io.file]::readalltext($file.fullname)
+		$match = [regex]::match($content, $pattern)
+		if ($match.success)
+		{
+			write-host $file.fullname -foregroundcolor green
+		}
+	}
+	cd $dir
+}
+
+contentsearch $regexpattern $searchpath $filetype
